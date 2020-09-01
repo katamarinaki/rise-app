eb0cc352be9f501747fea21a7cede9c617e33127
Alexander Belokon <ikatamarinaki@gmail.com>
2020-09-01 10:32:06 +0300

    bind twitter streams from backend to trending causes screen

--- src/features/cause-selector/causes-container.js
+++ src/features/cause-selector/causes-container.js
@@ -1,55 +1,91 @@
 import { Container } from '@src/shared/container'
-import React, { useEffect } from 'react'
+import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react'
 import { Button, useWindowDimensions } from 'react-native'
 import styled from 'styled-components/native'
 import { BubbleChart } from './bubble-chart'
 import { useStoreon } from 'storeon/react'
+import WS from 'react-native-websocket'
+import { Loader } from '@src/shared/loader'
+
+const keywords = ['Belarus', 'Trump']
 
 export const CausesContainer = () => {
+  const wsRef = useRef(null)
   const { width, height } = useWindowDimensions()
+  const [isLoaded, setIsLoaded] = useState(false)
   const { dispatch, trendingCauses, selectedCauses } = useStoreon(
     'trendingCauses',
     'selectedCauses',
   )
 
   useEffect(() => {
-    animateCircle()
+    if (trendingCauses.length === keywords.length && !isLoaded) {
+      animate()
+      setIsLoaded(true)
+    }
+  }, [isLoaded, trendingCauses])
+
+  const needUpdate = useMemo(() => {
+    return (
+      trendingCauses.filter(cause => cause.value === cause.maxValue).length !==
+      trendingCauses.length
+    )
+  }, [trendingCauses])
+
+  const animate = useCallback(() => {
+    if (needUpdate) {
+      dispatch('trendingCauses/update')
+      requestAnimationFrame(animate)
+    }
+  }, [needUpdate])
+
+  const onBubbleSelect = useCallback(bubble => {
+    dispatch('trendingCauses/selectCause', bubble.data.id)
   }, [])
 
-  const animateCircle = () => {
-    const updatedCause = trendingCauses[0]
-    dispatch('trendingCauses/updateValue', {
-      id: updatedCause.id,
-      value: updatedCause.value + 1,
-    })
-    if (updatedCause.value < 200) {
-      // requestAnimationFrame(animateCircle)
-    }
-  }
-
-  const onBubbleSelect = bubble => {
-    dispatch('trendingCauses/selectCause', bubble.data.id)
-  }
-
   return (
-    <Container>
-      <TitleView>
-        <Title>
-          Please select at least one cause that you find important for you
-        </Title>
-      </TitleView>
-      <BubbleChart
-        width={width}
-        height={height - 200}
-        dataSet={trendingCauses}
-        onBubblePress={onBubbleSelect}
+    <>
+      {isLoaded ? (
+        <Container>
+          <TitleView>
+            <Title>
+              Please select at least one cause that you find important for you
+            </Title>
+          </TitleView>
+          <BubbleChart
+            width={width}
+            height={height - 200}
+            dataSet={trendingCauses}
+            onBubblePress={onBubbleSelect}
+          />
+          <Button
+            onPress={() => {}}
+            title="Continue"
+            disabled={selectedCauses.length === 0}
+          />
+        </Container>
+      ) : (
+        <Loader />
+      )}
+      <WS
+        ref={wsRef}
+        url="ws://localhost:5000/twitter"
+        onOpen={() => {
+          keywords.forEach(keyword => {
+            wsRef.current.send(
+              JSON.stringify({
+                command: 'getTweets',
+                args: { q: keyword, count: '10' },
+              }),
+            )
+          })
+        }}
+        onMessage={message => dispatch('trendingCauses/newData', message)}
+        onError={console.log}
+        onClose={console.log}
+        // reconnect
       />
-      <Button
-        onPress={() => {}}
-        title="Continue"
-        disabled={selectedCauses.length === 0}
-      />
-    </Container>
+    </>
   )
 }
 
--- src/store/trending-causes.js
+++ src/store/trending-causes.js
@@ -1,17 +1,31 @@
-export default initialCauses => store => {
+export default store => {
   store.on('@init', () => ({
-    trendingCauses: initialCauses,
+    trendingCauses: [],
     selectedCauses: [],
   }))
-  store.on(
-    'trendingCauses/updateValue',
-    ({ trendingCauses }, { id, value }) => {
-      const newCauses = trendingCauses
-      const index = newCauses.findIndex(cause => cause.id === id)
-      newCauses[index].value = value
-      return { trendingCauses: newCauses }
-    },
-  )
+  store.on('trendingCauses/newData', ({ trendingCauses }, data) => {
+    const updatedCause = JSON.parse(data.data)
+    if (!updatedCause || updatedCause.error) {
+      return { trendingCauses }
+    }
+    const newCauses = [...trendingCauses]
+    const existedCauseIndex = newCauses.findIndex(
+      cause => cause.id === updatedCause.id,
+    )
+    if (existedCauseIndex !== -1) {
+      newCauses[existedCauseIndex] = {
+        ...newCauses[existedCauseIndex],
+        maxValue: updatedCause.value,
+      }
+    } else {
+      newCauses.push({
+        ...updatedCause,
+        maxValue: updatedCause.value,
+        isSelected: false,
+      })
+    }
+    return { trendingCauses: newCauses }
+  })
   store.on('trendingCauses/selectCause', ({ trendingCauses }, id) => {
     const newCauses = trendingCauses
     const index = newCauses.findIndex(cause => cause.id === id)
@@ -19,4 +33,53 @@ export default initialCauses => store => {
     const selectedCauses = newCauses.filter(cause => cause.isSelected)
     return { trendingCauses: newCauses, selectedCauses }
   })
+  store.on('trendingCauses/update', ({ trendingCauses }) => {
+    const newCauses = [...trendingCauses]
+    newCauses.map(cause => {
+      if (cause.value < cause.maxValue) {
+        cause.value += 0.5
+        return cause
+      }
+      if (cause.value > cause.maxValue) {
+        cause.value -= 0.5
+        return cause
+      }
+      return cause
+    })
+    return {
+      trendingCauses: newCauses,
+    }
+  })
 }
+
+// if (needCauseUpdate) {
+//   const newCauses = [...trendingCauses]
+//   newCauses.map(cause => {
+//     if (cause.value < cause.maxValue) {
+//       cause.value += 0.5
+//       return cause
+//     }
+//     if (cause.value > cause.maxValue) {
+//       cause.value -= 0.5
+//       return cause
+//     }
+//     return cause
+//   })
+//   const needUpdate =
+//     newCauses.filter(cause => cause.value === cause.maxValue).length !==
+//     newCauses.length
+//   return { trendingCauses: newCauses, needCauseUpdate: needUpdate }
+// } else {
+//   const indexToUpdate = random(0, trendingCauses.length - 1, false)
+//   const newCauses = [...trendingCauses]
+//   if (newCauses[indexToUpdate].maxValue > 200) {
+//     newCauses[indexToUpdate].maxValue = Math.floor(
+//       newCauses[indexToUpdate].maxValue - random(20, 40, false),
+//     )
+//   } else if (newCauses[indexToUpdate].maxValue > 10) {
+//     newCauses[indexToUpdate].maxValue = Math.floor(
+//       newCauses[indexToUpdate].maxValue + random(20, 40, false),
+//     )
+//   }
+//   return { trendingCauses: newCauses, needCauseUpdate: true }
+// }
